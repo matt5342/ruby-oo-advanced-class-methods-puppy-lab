8177d0f8da842b62258636d4f36591651654aa5f
Matt <mattfsewell@gmail.com>
2021-01-13 18:17:22 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        
+        save
+       
+
+    end
+    def save
+        @@all << self
+    end
+    def self.all
+        @@all.each do |dog|
+            dog
+        end
+    end
+    def self.clear_all
+        @@all = []
+    end
+    def self.print_all
+        @@all.each do |item|
+            puts item.name
+        end
+    end
+    
+
+
+end
